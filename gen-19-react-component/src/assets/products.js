b37b39881c6132b768d71955515d6a8c6eef2622
zahrotuladillah <zahrotuladillah@gmail.com>
2024-02-06 10:36:03 +0700

    separate dummy data

---
+++ gen-19-react-component/src/assets/products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        category: "best",
+        image:"images/product-1.0.webp",
+        name: "Double pocket knit jacket with long sleeves",
+        price: "¥2,990",
+    },
+    {
+        category:"autumn",
+        image:"images/product-2.0.webp",
+        name:"Half-Zip Sweater (Long Sleeve) (Border)",
+        price:"¥2,990"
+    },
+    {
+        category:"autumn",
+        image:"images/product-3.0.webp",
+        name:"Fair Isle Sweater (Long Sleeve) + E",
+        price:"¥990"
+    },
+    {
+        category:"daily",
+        image:"images/product-8.0.webp",
+        name:"Bouclé knit jacket (long sleeves) + E (can be set up)",
+        price:"¥2,990"
+    },
+    {
+        category:"best",
+        image:"images/product-4.0.webp",
+        name:"Mesh Sweater (Long Sleeve)",
+        price:"¥1,990"
+    },
+    {
+        category:"autumn",
+        image:"images/product-5.0.webp",
+        name:"Puffy Touch Low Gauge Crew Neck Sweater (Long Sleeve)",
+        price:"¥790"
+    },
+    {
+        category:"winter",
+        image:"images/product-13.1.webp",
+        name:"Low Gauge Sweater with Long Sleeves (Gradient)",
+        price:"¥1,990"
+    },
+]
+
+export {products}
