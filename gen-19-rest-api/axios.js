9ed9c36607f68740475829bc7358c4f30cdc534d
zahrotuladillah <zahrotuladillah@gmail.com>
2024-02-13 11:36:14 +0700

    add rest-api

---
+++ gen-19-rest-api/axios.js
@@ -0,0 +1,54 @@
+const getAllRecipes = async() => {
+    try{
+        const response = await axios.get(`https://dummyjson.com/recipes`)
+        console.log("Get All")
+        console.log(response.data.recipes)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+getAllRecipes()
+
+
+const getChocolateRecipes = async() => {
+    try{
+        const response = await axios.get(`https://dummyjson.com/recipes/search?q=Chocolate`)
+        console.log("Get Chocolate")
+        console.log(response.data.recipes)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+getChocolateRecipes()
+
+const payloadUpdate = {mealType: ["Breakfast"],}
+
+const updateType = async(id,payload) => {
+    try{
+        console.log(payload)
+        const response = await axios.put(`https://dummyjson.com/recipes/${id}`, payload)
+        console.log("Update Type")
+        console.log(response)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+updateType(2,payloadUpdate)
+
+
+const deleteRecipe = async(id) => {
+    try{
+        const response = await axios.delete(`https://dummyjson.com/recipes/${id}`)
+        console.log("Delete Recipe")
+        console.log(response.data.recipes)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+deleteRecipe(5)
+
+
