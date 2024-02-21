6974166f714f691bace1682ec207750a3f657175
zahrotuladillah <zahrotuladillah@gmail.com>
2024-02-21 23:27:58 +0700

    add array manipulation

---
+++ gen-19-array-manipulation/index.js
@@ -0,0 +1,360 @@
+const data = {
+    "product" : [
+        {
+            "id": 1,
+            "category": "best seller",
+            "name": "Double pocket knit jacket with long sleeves",
+            "price": "¥2,990",
+            "description":"",
+            "dataPrev" : [
+                {
+                    "dataPreview" : "../images/product-1.0.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-1.0.1.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-1.0.2.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-1.0.3.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-1.0.4.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-1.1.webp",
+                    "color" : "chocolate"
+                },
+                {
+                    "dataPreview" : "../images/product-1.2.webp",
+                    "color" : "black"
+                }
+            ],
+            
+            "dataSize" : ["S", "M", "L", "XL"],
+            
+            "dataColor" : [
+                {
+                    "name" : "cream",
+                    "hexCode" : "#FAF6F3"
+                },
+                {
+                    "name" : "chocolate",
+                    "hexCode" : "#836949"
+                },
+                {
+                    "name" : "black",
+                    "hexCode" : "#FFFFFF"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "category":"autumn",
+            "name":"Half-Zip Sweater (Long Sleeve) (Border)",
+            "price":"¥2,990",
+            "description":"",
+            "dataPrev" : [
+                {
+                    "dataPreview" : "../images/product-2.0.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-2.1.webp",
+                    "color" : "black"
+                },
+                {
+                    "dataPreview" : "../images/product-2.1.1.avif",
+                    "color" : "black"
+                },
+                {
+                    "dataPreview" : "../images/product-2.1.2.avif",
+                    "color" : "black"
+                },
+                {
+                    "dataPreview" : "../images/product-2.1.3.avif",
+                    "color" : "black"
+                }
+            ],
+            
+            "dataSize" : ["S", "M", "L", "XL"],
+            
+            "dataColor" : [
+                {
+                    "name" : "cream",
+                    "hexCode" : "#FAF6F3"
+                },
+                {
+                    "name" : "black",
+                    "hexCode" : "#FFFFFF"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "category":"daily",
+            "name":"Bouclé knit jacket (long sleeves) + E (can be set up)",
+            "price":"¥2,990",
+            "description":"",
+            "dataPrev" : [
+                {
+                    "dataPreview" : "../images/product-8.0.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-8.0.1.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-8.0.2.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-8.0.3.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-8.1.webp",
+                    "color" : "black"
+                },
+                {
+                    "dataPreview" : "../images/product-8.0.4.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-8.1.1.avif",
+                    "color" : "black"
+                }
+            ],
+            
+            "dataSize" : ["S", "M", "L", "XL"],
+            
+            "dataColor" : [
+                {
+                    "name" : "cream",
+                    "hexCode" : "#FAF6F3"
+                },
+                {
+                    "name" : "black",
+                    "hexCode" : "#FFFFFF"
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "category":"best seller",
+            "name":"Mesh Sweater (Long Sleeve)",
+            "price":"¥1,990",
+            "description":"",
+            "dataPrev" : [
+                {
+                    "dataPreview" : "../images/product-4.0.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-4.1.webp",
+                    "color" : "yellow"
+                },
+                {
+                    "dataPreview" : "../images/product-4.1.1.avif",
+                    "color" : "yellow"
+                },
+                {
+                    "dataPreview" : "../images/product-4.1.2.avif",
+                    "color" : "yellow"
+                },
+                {
+                    "dataPreview" : "../images/product-4.1.3.avif",
+                    "color" : "yellow"
+                },
+                {
+                    "dataPreview" : "../images/product-4.2.webp",
+                    "color" : "black"
+                },
+                {
+                    "dataPreview" : "../images/product-4.0.1.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-4.1.4.avif",
+                    "color" : "yellow"
+                },
+                {
+                    "dataPreview" : "../images/product-4.2.1.avif",
+                    "color" : "black"
+                }
+            ],
+            
+            "dataSize" : ["S", "M", "L", "XL"],
+            
+            "dataColor" : [
+                {
+                    "name" : "cream",
+                    "hexCode" : "#FAF6F3"
+                },
+                {
+                    "name" : "yellow",
+                    "hexCode" : "#EBCC7E"
+                },
+                {
+                    "name" : "black",
+                    "hexCode" : "#FFFFFF"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "category":"winter",
+            "name":"Middle Gauge Crewneck Cardigan (Long Sleeve)",
+            "price":"¥790",
+            "description":"",
+            "dataPrev" : [
+                {
+                    "dataPreview" : "../images/product-6.0.webp",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-6.0.1.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-6.0.2.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-6.0.3.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-6.1.webp",
+                    "color" : "blue"
+                },
+                {
+                    "dataPreview" : "../images/product-6.2.webp",
+                    "color" : "purple"
+                },
+                {
+                    "dataPreview" : "../images/product-6.3.webp",
+                    "color" : "gray"
+                },
+                {
+                    "dataPreview" : "../images/product-6.0.4.avif",
+                    "color" : "cream"
+                },
+                {
+                    "dataPreview" : "../images/product-6.1.1.avif",
+                    "color" : "blue"
+                },
+                {
+                    "dataPreview" : "../images/product-6.2.1.avif",
+                    "color" : "purple"
+                },
+                {
+                    "dataPreview" : "../images/product-6.3.1.avif",
+                    "color" : "gray"
+                }
+            ],
+            
+            "dataSize" : ["S", "M", "L", "XL"],
+            
+            "dataColor" : [
+                {
+                    "name" : "cream",
+                    "hexCode" : "#FAF6F3"
+                },
+                {
+                    "name" : "blue",
+                    "hexCode" : "#9DB1C8"
+                },
+                {
+                    "name" : "purple",
+                    "hexCode" : "#AA9BB4"
+                },
+                {
+                    "name" : "gray",
+                    "hexCode" : "#505554"
+                }
+            ]
+        }
+    ]
+}
+
+// console.log(data)
+
+const newProduct = {
+    "id": 6,
+    "category":"best seller",
+    "name":"Stripes Cardigan",
+    "price":"¥2,990",
+    "description":"best cardigan ever",
+    "dataPrev" : [
+        {
+            "dataPreview" : "https://image.uniqlo.com/GU/ST3/AsianCommon/imagesgoods/350126/item/goods_01_350126.jpg?width=700",
+            "color" : "cream"
+        },
+        {
+            "dataPreview" : "https://image.uniqlo.com/GU/ST3/AsianCommon/imagesgoods/350126/sub/goods_350126_sub10.jpg?width=700",
+            "color" : "cream"
+        },
+        {
+            "dataPreview" : "https://image.uniqlo.com/GU/ST3/AsianCommon/imagesgoods/350126/sub/goods_350126_sub18.jpg?width=700",
+            "color" : "cream"
+        },
+        {
+            "dataPreview" : "https://image.uniqlo.com/GU/ST3/AsianCommon/imagesgoods/350126/item/goods_05_350126.jpg?width=700",
+            "color" : "black"
+        }
+    ],
+    
+    "dataSize" : ["S", "M", "L", "XL"],
+    
+    "dataColor" : [
+        {
+            "name" : "cream",
+            "hexCode" : "#FAF6F3"
+        },
+        {
+            "name" : "black",
+            "hexCode" : "#FFFFFF"
+        }
+    ]
+}
+
+function addProduct(prod){
+    data.product.push(prod)
+    return data.product
+}
+
+function removeProduct(id){
+    const filter = data.product.filter(item=> item.id !== id)
+    return filter
+}
+
+function updateProduct(id, key, value){
+    const found = data.product.find(item => item.id === id); // Using find instead of filter
+    if (found) {
+        found[key] = value; // Update the property using bracket notation
+    } else {
+        console.error("Product not found with ID:", id);
+    }
+    return data.product;
+}
+
+console.log("\n===================ADD===================\n")
+console.log(addProduct(newProduct))
+// addProduct(newProduct)
+// console.log(removeProduct(6))
+console.log("\n===================UPDATE SIZE===================\n")
+console.log(updateProduct(6, "dataSize", ["S", "M", "L"]))
+console.log("\n===================UPDATE CATEGORY===================\n")
+console.log(updateProduct(6, "category", "winter"))
+console.log("\n===================REMOVE===================\n")
+console.log(removeProduct(6))
+
+
